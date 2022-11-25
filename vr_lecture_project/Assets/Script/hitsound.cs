7d549c6cd1369a60a1ad043240b0b685b7fc8d8d
dapin <89961651+dapin1490@users.noreply.github.com>
2022-11-25 14:04:14 +0900

    11주 수업 내용 중 새 에셋이랑 스크립트만 올림
    오류가 났는데 어디서 망가졌는지 몰라서 나머지는 다 discard할 생각

---
+++ vr_lecture_project/Assets/Script/hitsound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitsound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void OnCollisionEnter()
+
+    {        
+            audioSource.Play(); //오디오 재생
+    }
+}
+
