fa8c60a2932689da9e4ce809b4c45db7ddf1ee11
dapin1490 <dapin1490@gmail.com>
2022-12-20 17:35:54 +0900

    씬 재시작 기능 추가

---
+++ vr_study_Wack A Wuggy/Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    int sceneIndex;
+    int frame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        frame = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            frame += 1;
+        }
+
+        if (frame >= 1000)
+        {
+            frame = 0;
+            SceneManager.LoadScene(sceneIndex);
+            print("reload scene");
+        }
+    }
+}
